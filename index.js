dae23cb4257f594f5e2a719009dd0556df7f5dce
Raymond Gregory <vinedsel@gmail.com>
2018-04-09 05:20:16 -0500

    Created and set up JS file.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+
+const app = express();
+
+const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+
+
+
+
+
+
+
+app.listen( process.env.PORT || 3000, ()=>{
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
